c2807cbec1a4050bcbb2d1282729922ff6dc6ffc
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-03-31 09:20:25 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://c.tenor.com/NvYI5wQNfgUAAAPo/nakano-yotsuba-pout.mp4";
+			"https://c.tenor.com/Lamvbbk-XRUAAAPo/iron-man2-tony-stark.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
