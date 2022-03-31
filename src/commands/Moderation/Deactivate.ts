e0f821ddb1451000ef20a5f64558dca251b121d5
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-03-31 06:21:16 +0530

    Update Deactivate.ts

--- src/commands/Moderation/Deactivate.ts
+++ src/commands/Moderation/Deactivate.ts
@@ -19,10 +19,10 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         const type = joined.trim().toLowerCase() as toggleableGroupActions
         if (!Object.values(toggleableGroupActions).includes(type))
-            return void M.reply(`🟥 Invalid Option: *${this.client.util.capitalize(type)}*`)
+            return void M.reply(`🟥 𝕨𝕙𝕠 𝕞𝕒𝕕𝕖 𝕪𝕠𝕦 𝕒𝕕𝕞𝕚𝕟: *${this.client.util.capitalize(type)}*`)
         const data = await this.client.getGroupData(M.from)
-        if (!data[type]) return void M.reply(`🟨 *${this.client.util.capitalize(type)}* is already *inactived*, Baka!`)
+        if (!data[type]) return void M.reply(`🟨 *${this.client.util.capitalize(type)}* 𝕛𝕖𝕣𝕜 𝕠𝕡𝕖𝕟 𝕪𝕠𝕦𝕣 𝕖𝕪𝕖𝕤 *inactived*, 𝕓𝕒𝕜𝕝𝕠𝕝!`)
         await this.client.DB.group.updateOne({ jid: M.from }, { $set: { [type]: false } })
-        return void M.reply(`🟩 *${this.client.util.capitalize(type)}* is now inactive`)
+        return void M.reply(`🟩 *${this.client.util.capitalize(type)}* 𝕚 𝕒𝕞 𝕘𝕠𝕚𝕟𝕘 𝕕𝕠𝕨𝕟 𝕕𝕠𝕨𝕟 𝕚𝕟𝕒𝕔𝕥𝕚𝕧𝕖`)
     }
 }
