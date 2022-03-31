61f0c9f8a3ebcf61ad28f214fa8a8e31722d5077
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-03-31 09:18:48 +0530

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -29,13 +29,13 @@ export default class Command extends BaseCommand {
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
 		const gifs = [
-			"https://c.tenor.com/qGBj-9nx7rsAAAPo/what-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/mMRNByRo8XMAAAPo/nakano-yotsuba-disturbance.mp4",
-			"https://c.tenor.com/yQ_zLRi6zUkAAAPo/yotsuba-nakano.mp4",
-			"https://c.tenor.com/DpLoI6rRq4YAAAPo/nakano-yotsuba-ribbon.mp4",
-			"https://c.tenor.com/dEnq15fNv6kAAAPo/aaaa-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/i1fRMMMvu38AAAPo/yotsuba-nakano-the-quintessential-quintuplets.mp4",
-			"https://c.tenor.com/l2F9LrkJEKYAAAPo/yotsuba-nakano.mp4",
+			"https://c.tenor.com/NBFsEqKdSr8AAAPo/iron-man-mark50gif-beam.mp4",
+			"https://c.tenor.com/wkjOrQp6eJgAAAPo/i-am-iron-man-iron-man.mp4",
+			"https://c.tenor.com/gG3tX97uQaQAAAPo/spiderman-iron-man.mp4",
+			"https://c.tenor.com/bovTjaa8kM4AAAPo/iron-man-tony-stark.mp4",
+			"https://c.tenor.com/OUfRywXq_VoAAAPo/iron-man-door.mp4",
+			"https://c.tenor.com/qcJrw5CTlIcAAAPo/iron-man-awesome.mp4",
+			"https://c.tenor.com/Lamvbbk-XRUAAAPo/iron-man2-tony-stark.mp4",
 		];
 		const selected = gifs[Math.floor(Math.random() * gifs.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*⚡「YOTSUBA BROADCAST」⚡*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			const text = `*⚡「ⓜⓞⓑⓐ ⓑⓒⓐⓢⓣ」⚡*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
 			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
 				mimetype: Mimetype.gif,
 				caption: `${text}`,
