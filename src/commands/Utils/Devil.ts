76b57b70c510209c0ddeb3ed76fff43a28babb85
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-04-01 06:56:43 +0530

    Update Devil.ts

--- src/commands/Utils/Devil.ts
+++ src/commands/Utils/Devil.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			command: "devil",
 			description: `Get text image`,
 			aliases: ["devil"],
-			category: "creation",
+			category: "utils",
 			usage: `${client.config.prefix}devil`,
 			baseXp: 50,
 		});
