e4e717928214e3553b75836dcdeaff80b7ed342c
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-04-01 06:55:19 +0530

    Update Bp.ts

--- src/commands/Utils/Bp.ts
+++ src/commands/Utils/Bp.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
 			command: "bp",
 			description: `Get text image`,
 			aliases: ["bp"],
-			category: "creation",
+			category: "utils",
 			usage: `${client.config.prefix}bp`,
 			baseXp: 50,
 		});
