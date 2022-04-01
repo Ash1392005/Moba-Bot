340af0f243e3d302f1139f42b8a9254e33e27645
Ash1392005 <95693441+Ash1392005@users.noreply.github.com>
2022-04-01 07:03:56 +0530

    Update balloon.ts

--- src/commands/Utils/balloon.ts
+++ src/commands/Utils/balloon.ts
@@ -13,7 +13,7 @@ export default class Command extends BaseCommand {
 		super(client, handler, {
 			command: "balloon",
 			description: `Get text image`,
-			category: "creation",
+			category: "utils",
 			usage: `${client.config.prefix}balloon`,
 			baseXp: 50,
 		});
